560e74ecbe56571b00e99fa2eb02a69278ab4a6f
Yiseul0728 <yiseul0728@gmail.com>
2021-04-14 23:10:08 +0900

    save ejecting

--- src/main/java/com/example/demo/helloWorld/HelloSpringBoot.java
+++ src/main/java/com/example/demo/helloWorld/HelloSpringBoot.java
@@ -1,11 +1,11 @@
 package com.example.demo.helloWorld;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController	
 public class HelloSpringBoot {
-	@PostMapping(value = "/api")
+	@GetMapping(value = "/api")
 	public String helloSpring() {
 		System.out.println("Hello Spring Boot Controller");
 		return "Welcome in Spring Boot";
